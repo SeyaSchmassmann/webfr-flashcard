a564ec0ef63290313751c250f7851258b3808dd2
seya.schmassmann <seya.schmassmann@diartis.ch>
2024-10-10 20:06:12 +0200

    implement exercise week 4

---
+++ src/main/java/ch/fhnw/webfr/flashcard/web/IndexController.java
@@ -0,0 +1,18 @@
+package ch.fhnw.webfr.flashcard.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    // redirect to /questionnaires
+    @GetMapping
+    public String index() {
+        return "redirect:/questionnaires";
+    }
+    
+}
--- src/main/java/ch/fhnw/webfr/flashcard/web/QuestionnaireController.java
+++ src/main/java/ch/fhnw/webfr/flashcard/web/QuestionnaireController.java
@@ -1,56 +1,50 @@
 package ch.fhnw.webfr.flashcard.web;
 
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import ch.fhnw.webfr.flashcard.domain.Questionnaire;
 import ch.fhnw.webfr.flashcard.persistance.QuestionnaireRepository;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
+@RequestMapping("/questionnaires")
 public class QuestionnaireController {
     
     @Autowired
     private QuestionnaireRepository questionnaireRepository;
 
-    @RequestMapping("/questionnaires")
-    public void findAll(HttpServletResponse response, HttpServletRequest request) throws IOException {
+    @GetMapping
+    public String findAll(Model model) {
 		List<Questionnaire> questionnaires = questionnaireRepository.findAll();
-		
-		PrintWriter writer = response.getWriter();
-		writer.append("<html><head><title>Example</title></head><body>");
-		writer.append("<h3>Fragebögen</h3>");
-		
-		for (Questionnaire questionnaire : questionnaires) {
-			
-			String url = request.getContextPath() + request.getServletPath();
-			url = url + "/" + questionnaire.getId().toString();
-			
-			writer.append("<p><a href='" + response.encodeURL(url) + "'>" + questionnaire.getTitle() + "</a></p>");
-		}
-		
-		writer.append("</body></html>");
+        model.addAttribute("questionnaires", questionnaires);
+		return "questionnaires/list";
     }
 
-    @RequestMapping("/questionnaires/{id}")
-    public void findById(@PathVariable("id") String id, HttpServletResponse response, HttpServletRequest request) throws IOException {
-        Questionnaire questionnaire = questionnaireRepository.findById(id).get();
-        
-        PrintWriter writer = response.getWriter();
-        writer.append("<html><head><title>Example</title></head><body>");
-        writer.append("<h3>Fragebogen</h3>");
-        writer.append("<p>" + questionnaire.getTitle() + "</p>");
-        writer.append("<p>" + questionnaire.getDescription() + "</p>");
-        String url = request.getContextPath();
-        writer.append("<p><a href='" + response.encodeURL(url) + "/questionnaires'>Back</a></p>");
-        writer.append("</body></html>");
+    @GetMapping(params = "form")
+    public String create(Model model) {
+        model.addAttribute("questionnaire", new Questionnaire());
+        return "questionnaires/create";
+    }
+
+    @GetMapping("/{id}")
+	public String findById(@PathVariable("id") String id, Model model) {
+		Optional<Questionnaire> questionnaire = questionnaireRepository.findById(id);
+		model.addAttribute("questionnaire", questionnaire.get());
+		return "questionnaires/show";
+    }
+
+    @PostMapping
+    public String create(Questionnaire questionnaire) {
+        questionnaireRepository.save(questionnaire);
+        return "redirect:/questionnaires";
     }
     
 }
